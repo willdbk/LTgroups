64483344a918fa65adeaae68814f8538a4088927
Will deBruynKops <willdbk@gmail.com>
2018-03-20 18:40:22 -0700

    Limited score function works

--- scripts.js
+++ scripts.js
@@ -1,20 +1,20 @@
 var trainees = [
-        "Jaya Blanchard",
-        "Eleanor Mildenstein",
-        "Amanda Bansiak",
-        "Kim Hancock",
-        "Tashi Brundige",
-        "Aida Muratoglu",
-        "Tor Parker",
-        "Arjun Mehta",
-        "Cooper Dart",
-        "Holden Turner",
-        "Dylan Atencio",
-        "Coleman Brockmeier",
-        "Jesse Dunn",
-        "Patrick Warner",
-        "George Walker",
-        "Dan Ralston"
+        {name:"Jaya Blanchard", gender:"f", year:1},
+        {name:"Eleanor Mildenstein", gender:"f", year:1},
+        {name:"Amanda Bansiak", gender:"f", year:2},
+        {name:"Kim Hancock", gender:"f", year:1},
+        {name:"Tashi Brundige", gender:"f", year:1},
+        {name:"Aida Muratoglu", gender:"f", year:1},
+        {name:"Tor Parker", gender:"f", year:1},
+        {name:"Arjun Mehta", gender:"m", year:1},
+        {name:"Cooper Dart", gender:"m", year:1},
+        {name:"Holden Turner", gender:"m", year:1},
+        {name:"Dylan Atencio", gender:"m", year:2},
+        {name:"Coleman Brockmeier", gender:"m", year:1},
+        {name:"Jesse Dunn", gender:"m", year:2},
+        {name:"Patrick Warner", gender:"m", year:2},
+        {name:"George Walker", gender:"m", year:1},
+        {name:"Dan Ralston", gender:"m", year:1}
     ];
 var num_groups = 0;
 var num_lods = 0;
@@ -26,16 +26,59 @@ function generate_groups() {
     var num_lod_form = document.getElementById("num-lods");
     num_lods = num_lod_form.options[num_lod_form.selectedIndex].value;
 
-    var groups = create_groups(num_groups);
+    var best_groups = create_groups();
+    var best_groups_score = get_score(best_groups);
 
+
+    for (var i = 0; i < 100; i++) {
+        var groups = create_groups();
+        var score = get_score(groups);
+        if(score < best_groups_score) {
+            best_groups = groups;
+            best_groups_score = score;
+        }
+    }
+
+
+    document.getElementById("groups").innerHTML = groups_toHTML(best_groups);
+
+}
+
+function get_score(groups) {
+    var score = 0;
+    score += gender_score(groups);
+    return score;
+}
+
+function gender_score(groups) {
+    var score = 0;
+    for (var i = 0; i < groups.length; i++) {
+        var group = groups[i];
+        var gender_diff = 0;
+        for(var j = 0; j < group.length; j++) {
+            var gender = trainees[group[j]].gender;
+            if(gender == 'f') {
+                gender_diff++;
+            }
+            else if(gender == 'm') {
+                gender_diff--;
+            }
+        }
+        score += Math.abs(gender_diff);
+    }
+    return score;
+}
+
+
+function groups_toHTML(groups) {
     var groupHTML = "";
 
     for (var i = 0; i < num_groups; i++) {
         groupHTML += `<p><b>Group ${i+1}:</b></p>\n<p>`;
         var group = groups[i];
         for (var j = 0; j < group.length; j++) {
-            var nextTrainee = trainees[group[j]];
-            groupHTML += `${nextTrainee}`;
+            var nextTrainee = trainees[group[j]].name;
+            groupHTML += nextTrainee;
             if (j != group.length - 1) {
                 groupHTML += ", ";
             }
@@ -45,11 +88,12 @@ function generate_groups() {
         }
         groupHTML += "</p>";
     }
-    document.getElementById("groups").innerHTML = groupHTML;
+    return groupHTML;
 }
 
+
 // returns a 2D array with num_groups # of groups
-function create_groups(num_groups) {
+function create_groups() {
     var array = [];
     for (var i = 0; i < trainees.length; i++) {
        array.push(i);
